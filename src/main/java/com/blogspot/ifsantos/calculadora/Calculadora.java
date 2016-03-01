f889eaf5ced95d787cd90350bee7a6d7525f745a
Ismael Santos <fs.ismael@gmail.com>
2016-03-01 00:02:26 -0300

    Criando projeto Maven com Junit4

---
+++ src/main/java/com/blogspot/ifsantos/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+package com.blogspot.ifsantos.calculadora;
+
+public class Calculadora {
+
+	public Float somar(Float a, Float b){
+		return null;
+		
+	}
+	public Float subtrair(Float a, Float b){
+		return null;
+		
+	}
+	public Float multiplicar(Float a, Float b){
+		return null;
+		
+	}
+	public Float dividir(Float a, Float b){
+		return b;
+		
+	}
+	
+}
---
+++ src/test/java/com/blogspot/ifsantos/calculadora/test/CalculadoraTest.java
@@ -0,0 +1,34 @@
+package com.blogspot.ifsantos.calculadora.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testSomar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtrair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiplicar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDividir() {
+		fail("Not yet implemented");
+	}
+
+}
