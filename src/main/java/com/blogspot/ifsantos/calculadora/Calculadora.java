4a9a6e9bbb6c3f783824dccc47be0cb3b108ead6
Ismael Santos <fs.ismael@gmail.com>
2016-03-03 01:33:17 -0300

    Completando os metodoas de teste. Testes com Max, Min , NaN, Infinete, lançamento de exception Ajustes nos Assert, utilizando equals, ao inves de true.

--- src/main/java/com/blogspot/ifsantos/calculadora/Calculadora.java
+++ src/main/java/com/blogspot/ifsantos/calculadora/Calculadora.java
@@ -2,25 +2,65 @@ package com.blogspot.ifsantos.calculadora;
 
 public class Calculadora {
 
-	public Float somar(Float a, Float b){
+	public Double somar(Double a, Double b){
 		if (b == null){
 			return a;
 		}
 		if (a == null){
 			return b;
 		}
+		
+		if (isMax(a) || isMax(b))
+			throw new ExcecaoAritmetica("problema na soma");
+		
 		return a+b;
 		
 	}
+	private boolean isMax(Double a) {
+		return a.compareTo(Double.MAX_VALUE)==0;
+	}
+	private boolean isMin(Double a) {
+		return a.compareTo(Double.MIN_VALUE)==0;
+	}
 	public Double subtrair(Double a, Double b){
+		if (b == null){
+			return a;
+		}
+		if (a == null){
+			return b;
+		}
+		if (isMax(a) || isMax(b))
+			throw new ExcecaoAritmetica("problema na soma");
+		
+		if (isMin(a) || isMin(b))
+			throw new ExcecaoAritmetica("problema na soma");
+		
 		return a-b;
 		
 	}
 	public Double multiplicar(Double a, Double b){
+		if (b == null){
+			return a;
+		}
+		if (a == null){
+			return b;
+		}
+		if (isMax(a) || isMax(b))
+			throw new ExcecaoAritmetica("problema na soma");
+		
+		if (isMin(a) || isMin(b))
+			throw new ExcecaoAritmetica("problema na soma");
+		
 		return a*b;
 		
 	}
 	public Double dividir(Double a, Double b){
+		if (a == null){
+			return null;
+		}
+		if (b == null){
+			return a;
+		}
 		return a/b;
 		
 	}
--- src/test/java/com/blogspot/ifsantos/calculadora/test/CalculadoraTest.java
+++ src/test/java/com/blogspot/ifsantos/calculadora/test/CalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.blogspot.ifsantos.calculadora.Calculadora;
+import com.blogspot.ifsantos.calculadora.ExcecaoAritmetica;
 
 public class CalculadoraTest {
 
@@ -18,84 +19,134 @@ public class CalculadoraTest {
 
 	@Test
 	public void testSomarOk() {
-		Float soma = c.somar(3f, 4f);
-		assertTrue("soma deve ser 7", 7f == soma);
+		Double soma = c.somar(3.0, 4.0);
+		assertEquals("soma deve ser 7", 7.0, soma, 0.0);
 	}
 	@Test
 	public void testSomarOkDecimal() {
-		Float soma = c.somar(3.33f, 4.44f);
-		assertTrue("soma deve ser 7.77", 7.77f == soma);
+		Double soma = c.somar(3.33, 4.44);
+		assertEquals("soma deve ser 7.77", 7.77, soma, 0.0000000000001);
 	}
 	@Test
 	public void testSomarANull() {
-		Float soma = c.somar(null, 4f);
-		assertTrue("soma com A=null deve ser igual a B (4)", 4f == soma);
+		Double soma = c.somar(null, 4.0);
+		assertEquals("soma com A=null deve ser igual a B (4)", 4.0, soma, 0.0);
 	}
 	@Test
 	public void testSomarBNull() {
-		Float soma = c.somar(3f, null);
-		assertTrue("soma com B=null deve ser igual a A (3)", 3f == soma);
+		Double soma = c.somar(3.0, null);
+		assertEquals("soma com B=null deve ser igual a A (3)", 3.0, soma, 0.0);
 	}
 	@Test
 	public void testSomarABNull() {
-		Float soma = c.somar(null, null);
+		Double soma = c.somar(null, null);
 		assertNull("soma com A e B nulos deve ser null", soma);
 	}
 	@Test
 	public void testSomarFracaoTendeAZero() {
-		Float soma = c.somar(6f, 0.000001f);
-		assertFalse("soma com Fracao deve ser diferente de soma com zero", 6f == soma);
-		
+		Double soma = c.somar(6.0, 0.000001);
+		assertTrue("soma com Fracao deve ser diferente de soma com zero", 6.0 != soma.doubleValue());
+	}
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testSomarMaxValue() {
+		Double ret = c.somar(Double.MAX_VALUE, 1.0);
+		System.out.println(ret);
 	}
 
 	@Test
-	public void testSubtrair() {
+	public void testSubtrairOK() {
 		Double subtracao = c.subtrair(13d, 3d);
-		assertEquals(0, Double.compare(subtracao, 10.0));
+		assertEquals("Resultado desta subtração deve ser 10.0",0, Double.compare(subtracao, 10.0));
+	}
+	@Test
+	public void testSubtrairANull() {
+		Double subtracao = c.subtrair(null, 3d);
+		assertEquals("Quando A é nulo, retorno deve ser B",0, Double.compare(subtracao, 3.0));
+	}
+	@Test
+	public void testSubtrairBNull() {
+		Double subtracao = c.subtrair(13d, null);
+		assertEquals("Quando B é nulo, retono deve ser A",0, Double.compare(subtracao, 13.0));
+	}
+	@Test
+	public void testSubtrairABNull() {
+		Double subtracao = c.subtrair(null, null);
+		assertNull("Quando A e B sao nulos retorno deve ser nulo",subtracao);
+	}
+	
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testSubtrairMinValue() {
+		c.subtrair(Double.MIN_VALUE, 1d);
 	}
 
 	@Test
-	public void testMultiplicar() {
+	public void testMultiplicarOK() {
 		Double mult = c.multiplicar(2.0, 3.0);
-		assertEquals(0, Double.compare(mult, 6.0));
+		assertEquals("Resultado desta multiplicação deve ser 6.0", 6.0, mult, 0.0);
+	}
+	
+	@Test
+	public void testMultiplicarANull() {
+		Double mult = c.multiplicar(null, 3.0);
+		assertEquals("Quando A é nulo, retorno deve ser B", 3.0, mult, 0.0);
+	}
+	@Test
+	public void testMultiplicarBNull() {
+		Double mult = c.multiplicar(2.0, null);
+		assertEquals("Quando B é nulo, retorno deve ser A", 2.0, mult, 0.0);
+	}
+	@Test
+	public void testMultiplicarABNull() {
+		Double mult = c.multiplicar(null,null);
+		assertNull("Quando A e B são nulos, retorno deve ser nulo", mult);
+	}
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testMultiplicarMaxValue() {
+		c.multiplicar(Double.MAX_VALUE, 2.0);
+	}
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testMultiplicarAlgoPorMaxValue() {
+		c.multiplicar(2.1, Double.MAX_VALUE);
 	}
 
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testMultiplicarMinValue() {
+		c.multiplicar(Double.MIN_VALUE, 2.0);
+	}
+	@Test(expected=ExcecaoAritmetica.class)
+	public void testMultiplicarAlgoPorMinValue() {
+		c.multiplicar(1.1,Double.MIN_VALUE);
+	}
+	
 	@Test
 	public void testDividirOk() {
 		Double div = c.dividir(3.0, 2.0);
-		System.out.println(div);
-		assertTrue(0==Double.compare(1.5, div));
+		assertEquals("Dividir 3 por 2 deve ser 1.5", 1.5, div, 0);
 	}
 	@Test
-	public void testDividirInfinite() {
+	public void testDividirPorZero() {
 		Double div = c.dividir(2.0, 0.0);
-		System.out.println(div);
-		assertTrue(Double.isInfinite(div));
+		assertTrue("Dividir zero deve ser infinito", Double.isInfinite(div));
 	}
 	@Test
-	public void testDividirNaN() {
+	public void testDividirZeroPorZero() {
 		Double div = c.dividir(0.0, 0.0);
-		System.out.println(div);
-		assertTrue(Double.isNaN(div));
+		assertEquals("Dividir 0 por 0 deve ser NaN",Double.NaN,(div),0);
 	}
 	@Test
 	public void testDividirANull() {
-		Double div = c.dividir(null, 0.0);
-		System.out.println(div);
-		assertTrue(0 == div);
+		Double div = c.dividir(null, 10.0);
+		assertNull("Dividir nulo por algo, deve ser nulo",div);
 	}
 	@Test
 	public void testDividirBNull() {
 		Double div = c.dividir(323d, null);
-		System.out.println(div);
-		assertTrue(323 == div);
+		assertEquals("Dividir A por nulo deve ser A", 323, div, 0.0);
 	}
 	@Test
 	public void testDividirABNull() {
 		Double div = c.dividir(null, null);
-		System.out.println(div);
-		assertNull(div);
+		assertNull("Dividir nulo por nulo deve ser nulo",div);
 	}
-
 }
 
