ecba505636e18a3fb91f95fc3281b22bb75565c5
Ismael Santos <fs.ismael@gmail.com>
2016-03-07 01:25:36 -0300

    Refactoring na classe de subtração, ao validar entrada com valores máximo e minimo;
    Refactoring de alguns nomes de metodos de teste, utilizando a
    notação "must + Ação que deve ocorrer"
    
    Adicionados plugins ao build maven: Controle de Encoding, controle de
    versão do fonte, e code coverage (JACOCO);

--- src/main/java/com/blogspot/ifsantos/calculadora/ExcecaoAritmetica.java
+++ src/main/java/com/blogspot/ifsantos/calculadora/ExcecaoAritmetica.java
@@ -3,23 +3,10 @@ package com.blogspot.ifsantos.calculadora;
 public class ExcecaoAritmetica extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	public ExcecaoAritmetica() {
-	}
-
-	public ExcecaoAritmetica(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
 	public ExcecaoAritmetica(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
-	public ExcecaoAritmetica(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
 	
 	
 }
